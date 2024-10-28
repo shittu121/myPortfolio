02c1ae83977da1dbdbd31e0035fad69985e183d7
shittu121 <shittufaruq6@gmail.com>
2024-10-28 23:58:31 +0000

    Update Navlinks

--- src/constants/socials.tsx
+++ src/constants/socials.tsx
@@ -2,22 +2,18 @@ import {
   IconBrandLinkedin,
   IconBrandTwitter,
   IconBrandYoutube,
+  IconBrandFiverr
 } from "@tabler/icons-react";
 
 export const socials = [
   {
-    href: "https://twitter.com/mannupaaji",
-    label: "Twitter",
-    icon: IconBrandTwitter,
-  },
-  {
-    href: "https://linkedin.com/in/manuarora28",
+    href: "https://www.linkedin.com/in/shittu-faruq-a37838273",
     label: "LinkedIn",
     icon: IconBrandLinkedin,
   },
   {
-    href: "https://youtube.com/maninthere",
-    label: "YouTube",
-    icon: IconBrandYoutube,
+    href: "https://www.fiverr.com/fredward121?source=gig_page&gigs=slug%3Aweb3js-frontend-developer-dapps-reactjs-blockchain-and-metamask-integration%2Cpckg_id%3A1&ref=is_seller_online%3Atrue",
+    label: "Fiverr",
+    icon: IconBrandFiverr,
   },
 ];
